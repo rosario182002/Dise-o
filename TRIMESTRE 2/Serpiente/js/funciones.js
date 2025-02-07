33a5408cffc7dbd349abd41e0177ba55aae9f1f2
rosario182002 <rosariodelgadomoreno17@gmail.com>
2025-02-07 18:57:03 +0100

    07/02/2025

--- TRIMESTRE 2/Serpiente/js/funciones.js
+++ TRIMESTRE 2/Serpiente/js/funciones.js
@@ -4,6 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = 650;
 canvas.height = 380;
 
+
+class cuerpoSerpiente{
+    constructor(radio, color,context,path){
+        this.radio = radio;
+        this.color = color;
+        this.context = context;
+        this.path = path;
+    }
+    dibujarCirculo(x,y,radio,color){
+        this.context.beginPath();
+        this.context.arc(x,y,radio,0,2*Math.PI);
+        this.context.fillStyle = color;
+        this.context.fill();
+        this.context.closePath();
+    }
+    dubujar(){
+        this.dibujarCirculo(this.path.slice(-1)[0].x, this.path.slice(-1)[0].y, this.radio, this.color);
+    }
+}
+
+
+
 class Serpiente{
     constructor(posicion, radio, color, velocidad, context){
         this.posicion = posicion;
@@ -12,11 +34,24 @@ class Serpiente{
         this.velocidad = velocidad;
         this.context = context;
         this.rotacion = 0;
+        this.body = [];
         this.teclas ={
             ArrowLeft: false,
             ArrowRight: false
         }
         this.teclado();
+    }
+    iniciarCuerpo(){
+        for(let i=0; i<3; i++){
+            let path = [];
+            for(let k=0; k=12; k++){
+                path.push({
+                    x:this.posicion.x,
+                    y:this.posicion.y
+                });
+            }
+            this.body.push(new cuerpoSerpiente(this.radio, this.color, this.context, path));
+        }
     }
 
     dibujarCirculo(x,y,radio,color){
@@ -38,6 +73,22 @@ class Serpiente{
         this.dibujarCirculo(this.posicion.x+3, this.posicion.y+8, this.radio-9, "white");
    
     }   
+
+    dibujarCuerpo(){
+        this.body[0].path.unshift({
+            x:this.posicion.x,
+            y:this.posicion.y
+        });
+        this.body[0].dibujar();
+
+        for(let i = 1; i<this.body.length; i++){
+            this.body[i].path.unshift(this.body[i-1].path.pop());
+            this.body[i].dibujar();
+        }
+       // this.body[]
+    }
+
+
     dibujar(){
         this.context.save();
 
@@ -58,6 +109,8 @@ class Serpiente{
         if(this.teclas.ArrowRight){
             this.rotacion += 0.4;
         }
+        this.posicion.x += Math.cos(this.rotacion)*this.velocidad;
+        this.posicion.y += Math.sin(this.rotacion)*this.velocidad;
     }
 
 
@@ -86,7 +139,7 @@ class Serpiente{
 
 const serpiente = new Serpiente({x:200, y:200},11,"#feba39",1.5,ctx);
 
-
+serpiente.iniciarCuerpo();
 
 
 function fondo(){
@@ -110,4 +163,4 @@ function actualizar(){
 actualizar();
 
 
-//https://www.youtube.com/watch?v=Sl6YUIvwbAk 19:12
+//https://www.youtube.com/watch?v=Sl6YUIvwbAk 26:44
